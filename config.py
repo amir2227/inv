9ef23b81dda99db05dbf7d92f7718c42010fb339
Abbasi <A.abbasi@sarem.org>
2021-06-28 10:15:31 +0430

    add config.py

---
+++ config.py
@@ -0,0 +1,25 @@
+API_KEY = 'put your API key from kavenegar here'
+
+# Mysql configs
+MYSQL_HOST = 'localhost'
+MYSQL_USERNAME = 'test'
+MYSQL_PASSWORD = 'test'
+MYSQL_DB_NAME = 'invskills'
+
+
+# call back url from KaveNegar will look like
+# /v1/CALL_BACK_TOKEN/process
+CALL_BACK_TOKEN = 'CALL BACK TOKEN'
+
+# login cedentials
+USERNAME = 'amir'
+PASSWORD = 'pass'
+
+# generate one strong secret key for flask.
+SECRET_KEY = 'random long string with alphanumeric + #()*&'
+
+
+### Do not change below unless you know what you are doing
+UPLOAD_FOLDER = 'D:\python\ExcelToDB'
+ALLOWED_EXTENSIONS = {'xls'} 
+
