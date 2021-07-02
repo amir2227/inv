f062d50498f79da3d12c6598823b61bdacf89cdb
amir2227 <amir.reza2823@gmail.com>
2021-07-02 17:33:54 +0430

    add table GDP from excel file

--- import.py
+++ import.py
@@ -1,4 +1,5 @@
 from pandas import read_excel
+import numpy as np
 import config
 import MySQLdb
 
@@ -35,10 +36,10 @@ def import_to_database_from_excel(filepath):
         output.append(
             f'problem dropping and creating new table for logs in database; {e}')
 
-    # remove the serials table if exists, then create the new one
+    # remove the E_SCORE table if exists, then create the new one
     try:
-        cur.execute('DROP TABLE IF EXISTS excel;')
-        cur.execute("""CREATE TABLE excel (
+        cur.execute('DROP TABLE IF EXISTS E_SCORE;')
+        cur.execute("""CREATE TABLE E_SCORE (
             id INTEGER PRIMARY KEY,
             d_date VARCHAR(200),
             USD FLOAT,EUR FLOAT,GBP FLOAT,JPY FLOAT,NZD FLOAT,AUD FLOAT,
@@ -51,7 +52,25 @@ def import_to_database_from_excel(filepath):
         output.append(
             f'problem dropping and creating new table excel in database; {e}')
     
-    cur.execute("INSERT INTO logs VALUES ('db_filename', %s)", (filepath, ))
+    cur.execute("INSERT INTO logs VALUES ('db_filename', 'E_SCORE CREATED')")
+    db.commit()
+
+    try:
+        cur.execute('DROP TABLE IF EXISTS GDP;')
+        cur.execute("""CREATE TABLE GDP (
+            id INTEGER PRIMARY KEY,
+            d_date VARCHAR(200),
+            USD FLOAT,EUR FLOAT,GBP FLOAT,JPY FLOAT,NZD FLOAT,AUD FLOAT,
+            CHF FLOAT,CAD FLOAT,TRY FLOAT,MXN FLOAT,ZAR FLOAT,SEK FLOAT,
+            DKK FLOAT,PLN FLOAT,SGD FLOAT,CZK FLOAT,HKD FLOAT,HUF FLOAT,
+            NOK FLOAT,RUB FLOAT,THB FLOAT,CHN FLOAT);""")
+        db.commit()
+    except Exception as e:
+        print("problem dropping excel")
+        output.append(
+            f'problem dropping and creating new table excel in database; {e}')
+    
+    cur.execute("INSERT INTO logs VALUES ('db_filename', 'GDP CREATED')")
     db.commit()
 
     df = read_excel(filepath, 0)
@@ -62,11 +81,7 @@ def import_to_database_from_excel(filepath):
             CZK, HKD, HUF, NOK, RUB, THB, CHN) in df.iterrows():
         line_number += 1
         try:
-            print(f'''INSERT INTO excel VALUES ({line_number}, '{d_date}', {USD},
-                {EUR}, {GBP}, {JPY}, {NZD}, {AUD}, {CHF}, {CAD}, {TRY},
-                {MXN}, {ZAR}, {SEK}, {DKK}, {PLN}, {SGD}, {CZK}, {HKD},
-                {HUF}, {NOK}, {RUB}, {THB}, {CHN});''')
-            cur.execute(f'''INSERT INTO excel VALUES ({line_number}, '{d_date}',
+            cur.execute(f'''INSERT INTO E_SCORE VALUES ({line_number}, '{d_date}',
                 {USD},{EUR}, {GBP}, {JPY}, {NZD}, {AUD}, {CHF}, {CAD}, {TRY},
                 {MXN}, {ZAR}, {SEK}, {DKK}, {PLN}, {SGD}, {CZK}, {HKD},
                 {HUF}, {NOK}, {RUB}, {THB}, {CHN});''')
@@ -75,7 +90,39 @@ def import_to_database_from_excel(filepath):
             total_flashes += 1
             if total_flashes < MAX_FLASH:                
                 output.append(
-                    f'Error inserting line {line_number} from excel sheet , {e}')
+                    f'Error inserting line {line_number} from E_SCORE sheet , {e}')
+            elif total_flashes == MAX_FLASH:
+                output.append(f'Too many errors!')
+        if line_number % 1000 == 0:
+            try:
+                db.commit()
+            except Exception as e:
+                output.append(
+                    f'Problem commiting data into db at around record {line_number} (or previous 1000 ones); {e}')
+    db.commit()
+    df = read_excel(filepath, 1)
+    df1 = df.replace(np.nan, 'NULL', regex=True)
+    excel_counter = 1
+    line_number = 1
+    for _, (d_date, USD, EUR, GBP, JPY, NZD, AUD,
+            CHF, CAD, TRY, MXN, ZAR, SEK, DKK, PLN, SGD,
+            CZK, HKD, HUF, NOK, RUB, THB, CHN) in df1.iterrows():
+        line_number += 1
+        try:
+            print(f'''INSERT INTO E_SCORE VALUES ({line_number}, '{d_date}',
+                {USD},{EUR}, {GBP}, {JPY}, {NZD}, {AUD}, {CHF}, {CAD}, {TRY},
+                {MXN}, {ZAR}, {SEK}, {DKK}, {PLN}, {SGD}, {CZK}, {HKD},
+                {HUF}, {NOK}, {RUB}, {THB}, {CHN});''')
+            cur.execute(f'''INSERT INTO GDP VALUES ({line_number}, '{d_date}',
+                {USD},{EUR}, {GBP}, {JPY}, {NZD}, {AUD}, {CHF}, {CAD}, {TRY},
+                {MXN}, {ZAR}, {SEK}, {DKK}, {PLN}, {SGD}, {CZK}, {HKD},
+                {HUF}, {NOK}, {RUB}, {THB}, {CHN});''')
+            excel_counter += 1
+        except Exception as e:
+            total_flashes += 1
+            if total_flashes < MAX_FLASH:                
+                output.append(
+                    f'Error inserting line {line_number} from GDP sheet , {e}')
             elif total_flashes == MAX_FLASH:
                 output.append(f'Too many errors!')
         if line_number % 1000 == 0:
